a9ee237606433a1df6e0d6d4571441c8094a4766
Ian Koech <ianovic8@live.com>
2024-10-26 15:07:36 +0300

    User Login Guards

---
+++ src/users/guards/guards.guard.ts
@@ -0,0 +1,39 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class UserGuard implements CanActivate {
+  constructor(
+    private jwtToken: JwtService,
+    private reflector: Reflector,
+  ) {}
+
+  async canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    const auth = request.headers.authorization;
+
+    // Split at 'Bearer #####' and keep the token
+    const token = auth?.split(' ')[1];
+
+    // Get the user object from the JWT
+    const user = this.jwtToken.decode(token);
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    // Check if the token and permissions are valid, if not, throw an error
+    try {
+      await this.jwtToken.verifyAsync(token);
+      return true;
+    } catch (error) {
+      throw new UnauthorizedException('Expired token');
+    }
+  }
+}
